661069a04409b5285344cbfac1a10d6376071165
Cusimonster <Cusimanone1@gcc.edu>
2014-10-21 12:56:43 -0400

    admin and login pages have javascript validation for forms

---
+++ scripts/login.js
@@ -0,0 +1,8 @@
+function validateForm() {
+    var x = document.forms["loginForm"]["username"].value;
+    var y = document.forms["loginForm"]["password"].value;
+    if (x==null || x=="" || y==null || y=="") {
+        alert("Please enter a username and password");
+        return false;
+    }
+}
