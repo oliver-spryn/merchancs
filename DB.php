6999ca6f2ed5cdbb22f977fec359754206bf20b2
XIMrPantsIX <ximrpantsix@gmail.com>
2014-12-16 09:33:30 -0500

    database interactions with account page

--- DB.php
+++ DB.php
@@ -1,9 +1,13 @@
 <?php
 //Connect to the the database
-	$host = getenv("MERCHANCS_HOST") or die("The environment variable MERCHANCS_HOST must contain the MySQL host.");
-	$database = getenv("MERCHANCS_DB") or die("The environment variable MERCHANCS_DB must contain the name of the database.");
-	$dbuser = getenv("MERCHANCS_DB_USER") or die("The environment variable MERCHANCS_DB_USER must contain the user name to the database.");
-	$dbpass = getenv('MERCHANCS_DB_PASS') or die("The environment variable MERCHANCS_DB_PASS must contain the password to the database.");
+	//$host = getenv("MERCHANCS_HOST") or die("The environment variable MERCHANCS_HOST must contain the MySQL host.");
+	$host = 'localhost';
+	//$database = getenv("MERCHANCS_DB") or die("The environment variable MERCHANCS_DB must contain the name of the database.");
+	$database = 'merchancs';
+	//$dbuser = getenv("MERCHANCS_DB_USER") or die("The environment variable MERCHANCS_DB_USER must contain the user name to the database.");
+	$dbuser = 'root';
+	//$dbpass = getenv('MERCHANCS_DB_PASS') or die("The environment variable MERCHANCS_DB_PASS must contain the password to the database.");
+	$dbpass = '';
 	
 	$db = new MySQLi($host, $dbuser, trim($dbpass), $database);
 	
--- modules/AccountModule.php
+++ modules/AccountModule.php
@@ -22,33 +22,101 @@ class AccountView extends View
 	}
 	public function fetchAccountInfo() 
 	{
-		//Pull data from DB
-		$this->user = "User";
-		$this->email = "user123@gcc.edu";
-		$this->password = "asdfjkl";
-		$this->payment = "Credit Card|Master Card|XXXX-XXXX-XXXX-1234";
-		$this->shipping = "200 Campus Dr.|Grove City, PA 16127";
+		$host = 'localhost';
+		$database = 'merchancs';
+		$dbuser = 'root';
+		$dbpass = '';
+		
+		$db = new MySQLi($host, $dbuser, trim($dbpass), $database);
+		$accountDetailsQuery = "Select usr_UName, usr_Email, usr_Pass, usr_payment, usr_shipping from tbl_users where usr_UName = '". $_SESSION['uname']."';";
+		$result = $db->query($accountDetailsQuery);
+		if($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$this->user = $row['usr_UName'];
+				$this->email = $row['usr_Email'];
+				$this->password = $row['usr_Pass'];
+				$this->payment = $row['usr_payment'];
+				$this->shipping = $row['usr_shipping'];
+			}
+		}
+		else {
+			echo "0 results";
+		}
+			
+		
+		$db->close();
+	}
+		public $newPassword; 
+	public function changeEmail($newEmail) 
+	{
+		$host = 'localhost';
+		$database = 'merchancs';
+		$dbuser = 'root';
+		$dbpass = '';
+		
+		$db = new MySQLi($host, $dbuser, trim($dbpass), $database);
+		$query = "update tbl_users set usr_Email='" . $newEmail . "' where usr_UName = '". $_SESSION['uname'] . "';";
+		if($db->query($query)===TRUE) {
+			echo "Updated successfully";
+		}
+		else {
+			echo "Error updating";
+		}
+	}
+	public function changePassword($oldPass, $newPass)
+	{
+		$host = 'localhost';
+		$database = 'merchancs';
+		$dbuser = 'root';
+		$dbpass = '';
+		
+		$db = new MySQLi($host, $dbuser, trim($dbpass), $database);
+		$hashedOld = md5($oldPass);
+		$hashedNew = md5($newPass);
+		$query = "update tbl_users set usr_Pass='" . $hashedNew . "' where usr_UName = '". $_SESSION['uname'] . "' and usr_Pass='" . $hashedOld ."';";
+		if($db->query($query)===TRUE) {
+			echo "Updated successfully";
+		}
+		else {
+			echo "Error updating";
+		}
+	}
+	public function changePayment($newPayment)
+	{
+		$host = 'localhost';
+		$database = 'merchancs';
+		$dbuser = 'root';
+		$dbpass = '';
+		
+		$db = new MySQLi($host, $dbuser, trim($dbpass), $database);
+		$query = "update tbl_users set usr_payment='" . $newPayment . "' where usr_UName = '". $_SESSION['uname'] . "';";
+		if($db->query($query)===TRUE) {
+			echo "Updated successfully";
+		}
+		else {
+			echo "Error updating";
+		}
+	}
+	public function changeShipping($newShipping)
+	{
+		$host = 'localhost';
+		$database = 'merchancs';
+		$dbuser = 'root';
+		$dbpass = '';
+		
+		$db = new MySQLi($host, $dbuser, trim($dbpass), $database);
+		$query = "update tbl_users set usr_shipping='" . $newShipping . "' where usr_UName = '". $_SESSION['uname'] . "';";
+		if($db->query($query)===TRUE) {
+			echo "Updated successfully";
+		}
+		else {
+			echo "Error updating";
+		}
 	}
 }
 
 class AccountController extends Controller
 {
-	public $newPassword; 
-	public function changeEmail() 
-	{
-		//Make query to database
-	}
-	public function changePassword()
-	{
-		//Make query to database
-	}
-	public function changePayment()
-	{
-		//Make query to database
-	}
-	public function changeShipping()
-	{
-		//Make query to database
-	}
+
 	
 }
