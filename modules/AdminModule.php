b61a72fbefeb27e6374aa9007f3157001d106c3f
XIMrPantsIX <ximrpantsix@gmail.com>
2014-12-16 20:42:42 -0500

    final commit

--- modules/AdminModule.php
+++ modules/AdminModule.php
@@ -2,18 +2,67 @@
 require_once('View.php');
 require_once('Controller.php');
 
+
 class AdminModel extends Model
 {
 
 }
 
+class AdminItem
+{
+	public $image;
+	public $name;
+	public $description;
+	public $pageURL;
+	public $price;
+    public $category;
+    
+	public function __construct($name, $desc, $price, $image, $cat)//do I need $URL?
+	{
+		$this->image = $image;
+		$this->name = $name;
+		$this->description = $desc;
+		//$this->pageURL = $url;
+		$this->price = $price;
+        $this->category = $cat;
+	}
+}
+
 class AdminView extends View
 {	
+    public $adminItems;
 	protected function getData()
 	{
 		return array(
 			'page' => 'admin',
 		);			
+	}
+    
+    public function fetchAdminData()
+	{
+		//global $db;
+        $host = 'localhost';
+		$database = 'merchancs';
+		$dbuser = 'root';
+		$dbpass = '';
+		
+		$db = new MySQLi($host, $dbuser, trim($dbpass), $database);
+        
+        $this->adminItems = array();
+       
+        $allProducts = $db->query("SELECT * FROM `tbl_products`");
+        
+        $sizeOfTable = $allProducts->num_rows;
+        
+        
+        
+        if($allProducts->num_rows > 0) 
+        {
+			while($row = $allProducts->fetch_assoc()) 
+            {
+				$this->adminItems[] = new AdminItem($row['pr_Name'], $row['pr_Description'], $row['pr_Price'], $row['pr_Image'],$row['cat_ID']);
+			}
+		}
 	}
 }
 
--- scripts/admin.js
+++ scripts/admin.js
@@ -1,7 +1,12 @@
 function validateForm() {
-    var x = document.forms["addItemForm"]["newItemText"].value;
-    if (x==null || x=="") {
-        alert("Please enter a title for your new item");
+    var title = document.forms["addItemForm"]["newItemText"].value;
+    var desc = document.forms["addItemForm"]["itemDescriptionText"].value;
+    var price = document.forms["addItemForm"]["itemPriceText"].value;
+    var img = document.forms["addItemForm"]["itemImage"].value;
+    var cat = document.forms["addItemForm"]["itemCategoryText"].value;
+    
+    if (title ==null || title=="" || desc ==null || desc=="" || price ==null || price=="" || img ==null || img=="" || cat ==null || cat=="") {
+        alert("Please fill out all required fields");
         return false;
     }
 }
